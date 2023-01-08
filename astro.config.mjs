f1e417f6590a48043558e09e8d1dd02bc143d41f
svetlyr <radu.svetly@protonmail.com>
2023-01-08 22:54:57 +0200

    refactor(project): switch to yarn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://svetlyR.github.io",
+  site: "https://svetlyr.github.io",
 });
